dac35dbf93414b9c59c2acbf731d759ca281926b
Wooksang <today8934@gmail.com>
2021-11-19 15:08:59 +0900

    Study Settings Change Published Status and Change Path and Title

--- src/main/java/com/studyolle/study/form/StudyForm.java
+++ src/main/java/com/studyolle/study/form/StudyForm.java
@@ -1,6 +1,5 @@
 package com.studyolle.study.form;
 
-import lombok.Builder;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
@@ -9,6 +8,8 @@ import javax.validation.constraints.Pattern;
 
 @Data
 public class StudyForm {
+
+    public static final String VALID_PATH_PATTERN = "^[ㄱ-ㅎ가-힣a-z0-9_-]{2,20}$";
 
     @NotBlank
     @Length(min = 2, max = 20)
